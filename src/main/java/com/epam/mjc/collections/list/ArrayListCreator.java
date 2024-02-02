06cf26c6d9fe2bdf729837b67b0f6dc8be7839e5
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-02 20:27:39 +0600

    array list creator is completed

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+        int counter = 2;
+        for(int i = 0; i < sourceList.size(); i++){
+            if (counter == i){
+                result.add(sourceList.get(i));
+                result.add(sourceList.get(i));
+                counter+=3;
+            }
+        }
+        return result;
     }
 }
