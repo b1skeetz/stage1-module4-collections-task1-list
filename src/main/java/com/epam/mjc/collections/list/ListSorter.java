0abc0611f5192296c4d5053f5fbdf67fef7117d4
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-02 20:52:16 +0600

    all is done

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int result = (int) ((5 * Math.pow(Integer.parseInt(a), 2) + 3) - (5 * Math.pow(Integer.parseInt(b), 2) + 3));
+        if(result == 0){
+            return a.compareTo(b);
+        }
+        return result;
     }
 }
